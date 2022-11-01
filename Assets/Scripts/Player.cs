6dedf041b9e10169fcb3508ad2f66219c71bf206
KimiNoUso <90899255+479626@users.noreply.github.com>
2022-11-01 16:09:29 +0000

    Add project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private LayerMask platform;
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        CheckForInput();
+    }
+
+    private void CheckForInput()
+    {
+        Vector2 velocity = rb.velocity;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x = -3;
+            print("Move attempt [" + velocity + "]");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x = 3;
+            print("Move attempt [" + velocity + "]");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && PlayerIsGrounded())
+        {
+            velocity.y = 3f;
+            print("Move attempt [" + velocity + "]");
+        }
+
+        rb.velocity = velocity;
+    }
+
+    private bool PlayerIsGrounded()
+    {
+        float heightTest = .06f;
+        RaycastHit2D raycast = Physics2D.Raycast(boxCollider.bounds.center, Vector2.down, boxCollider.bounds.extents.y + heightTest, platform);
+        return raycast.collider != null;
+    }
+}
